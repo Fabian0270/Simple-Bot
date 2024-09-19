4274dd6037cc9728fa5d636be1237d754b9963f4
Fabian Olsson <144779113+Fabian0270@users.noreply.github.com>
2024-09-20 01:13:35 +0200

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,9 @@
+name = input("Hello! What is your name? ")
+print(f"Nice to meet you, {name}")
+age_input = input("How old are you?")
+age = int(age_input)
+bot_age = 100
+age_difference = bot_age - age  
+print(f"You are {age_difference} years younger than me. I'm {bot_age} years old!")
+color = input("What's your favorite color?")
+print(f"Oh,{color} is a beautiful color!")
